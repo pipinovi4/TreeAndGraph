32934bc2b5a076a9501e698647957c684b457e53
pipinovi4 <prustolivgra@gmail.com>
2024-11-09 18:15:48 +0100

    Was added new nodes for btrees and changed name of the director from "B+Trees" to "BPTrees"

--- Nodes/b_tree_node.js
+++ Nodes/b_tree_node.js
@@ -1,32 +1,15 @@
 class BTreeNode {
-    /**
-     * Constructor for a BTree node.
-     * @param {boolean} is_leaf - Indicates if the node is a leaf.
-     */
-    constructor(is_leaf = true) {
-        this.is_leaf = is_leaf;  // true if node is a leaf, false otherwise
-        this.keys = [];           // Array to store keys in the node
-        this.children = [];       // Array to store child pointers (empty if leaf)
+    constructor(t, is_leaf = true) {
+        this.t = t;
+        this.is_leaf = is_leaf;
+        this.keys = [];
+        this.children = [];
     }
 
-    /**
-     * Add a key to the node.
-     * @param {any} key - The key to add.
-     */
-    addKey(key) {
-        this.keys.push(key);
-        this.keys.sort((a, b) => a - b); // Sort keys after insertion to maintain order
-    }
+    _insertNonFull(key) {
+        const i = this.keys.length - 1
 
-    /**
-     * Remove a key from the node.
-     * @param {any} key - The key to remove.
-     */
-    removeKey(key) {
-        const index = this.keys.indexOf(key);
-        if (index !== -1) {
-            this.keys.splice(index, 1);
-        }
+        if
     }
 }
 
