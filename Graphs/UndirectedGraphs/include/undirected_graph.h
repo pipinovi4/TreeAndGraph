95d6f85d20f8be017c7c02c800cbf0472075ee7a
pipinovi4 <prustolivgra@gmail.com>
2024-12-08 13:14:09 +0100

    Was renamed directory from "UndirectedGraph" to "UndirectedGraphs"

---
+++ Graphs/UndirectedGraphs/include/undirected_graph.h
@@ -0,0 +1,9 @@
+#ifndef UNDIRECTED_GRAPH_H
+#define UNDIRECTED_GRAPH_H
+
+#pragma once
+
+
+#include "../source/undirected_graph.tpp"
+
+#endif //UNDIRECTED_GRAPH_H
---
+++ Graphs/UndirectedGraphs/source/undirected_graph.tpp
@@ -0,0 +1,6 @@
+#ifndef UNDIRECTED_GRAPH_TPP
+#define UNDIRECTED_GRAPH_TPP
+
+#include "../include/undirected_graph.h"
+
+#endif //UNDIRECTED_GRAPH_TPP
