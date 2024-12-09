7f8de75340672d27f0234b0a718929823f7b8fba
pipinovi4 <prustolivgra@gmail.com>
2024-12-09 01:26:38 +0100

    Added UndirectedGraph header file in C++.
    - Implemented the UndirectedGraph class definition in C++.
    - Added basic methods for managing vertices and edges.
    - Included methods for adding vertices, adding edges, checking if an edge exists, and displaying the graph.
    - Prepared the header for further expansion with traversal algorithms.

--- Graphs/UndirectedGraphs/include/undirected_graph.h
+++ Graphs/UndirectedGraphs/include/undirected_graph.h
@@ -2,7 +2,24 @@
 #define UNDIRECTED_GRAPH_H
 
 #pragma once
+#include <vector>
 
+template <typename T>
+class UndirectedGraph {
+public:
+    UndirectedGraph();
+
+    void addVertex(std::vector<T> vertex);
+
+    void addEdge(std::vector<T> vertex1, std::vector<T> vertex2);
+
+    bool hasEdge(std::vector<T> source, std::vector<T> target);
+
+    void print();
+
+private:
+    std::vector<T> _adjacency_list;
+};
 
 #include "../source/undirected_graph.tpp"
 
