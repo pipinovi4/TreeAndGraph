7eea7d895754b9f8149612a570237f9eba2f8dd5
pipinovi4 <prustolivgra@gmail.com>
2024-12-13 15:55:24 +0100

    Implement DFSGraph class in Python
    - Added `DFSGraph` class with methods for:
      - Adding vertices and edges.
      - Performing Depth-First Search (DFS) traversal starting from a specified vertex.
      - Displaying the adjacency list representation of the graph.
    
    This implementation supports directed graphs and includes an example demonstrating graph creation, DFS traversal, and displaying the graph structure.

--- Graphs/DFSGraphs/source/dfs_graph.py
+++ Graphs/DFSGraphs/source/dfs_graph.py
@@ -2,3 +2,33 @@ from collections import defaultdict, deque
 
 
 class DFSGraph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.vertices = set()
+
+    def add_vertex(self, vertex):
+        self.vertices.add(vertex)
+
+    def add_edge(self, u, v):
+        if u not in self.vertices:
+            self.add_vertex(u)
+        if v not in self.vertices:
+            self.add_vertex(v)
+
+        self.graph[u].append(v)
+
+    def dfs(self, vertex):
+        visited = set()
+        stack = deque()
+        stack.append(vertex)
+
+        while len(vertex) != 0:
+            current_vertex = stack.pop()
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+                for neighbor in self.graph[current_vertex]:
+                    stack.append(neighbor)
+
+    def display(self):
+        for vertex in self.vertices:
+            print(f"{vertex} -> {self.graph[vertex]}")
