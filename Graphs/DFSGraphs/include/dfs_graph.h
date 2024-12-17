f940af46e59f010032ec472c6ab9e7430c2db55c
pipinovi4 <prustolivgra@gmail.com>
2024-12-17 02:50:24 +0100

    Add DFSGraph header file for C++ implementation
    - Added `dfs_graph.h` defining the `Vertex` and `DFSGraph` classes.
    - `Vertex` class:
      - Stores a value and maintains a list of neighboring vertices.
      - Provides methods to add neighbors and retrieve the vertex value and neighbors.
    
    - `DFSGraph` class:
      - Supports adding vertices and edges.
      - Implements Depth-First Search (DFS) traversal.
      - Includes a method to display the graph structure.
    
    This header file lays the foundation for creating and managing directed graphs with DFS traversal functionality.

--- Graphs/DFSGraphs/include/dfs_graph.h
+++ Graphs/DFSGraphs/include/dfs_graph.h
@@ -2,12 +2,41 @@
 #define DFS_INCLUDE_H
 
 #pragma once
+#include <vector>
+#include <map>
+#include <set>
 
-class DFSGraph {
+template <typename T>
+class Vertex {
 public:
+    Vertex(T value);
+
+    void addNeighbor(Vertex<T>* neighbor);
+
+    const T& getValue() const;
+
+    const std::vector<Vertex<T>*>& getNeighbors() const;
 
 private:
+    T _value;
+    std::vector<Vertex<T>*> _neighbors;
+};
 
+template <typename T>
+class DFSGraph {
+public:
+    DFSGraph();
+
+    void add_vertex(Vertex<T>* vertex);
+
+    void add_edge(Vertex<T>* source, Vertex<T>* target);
+
+    void dfs(Vertex<T>* start_vertex);
+
+    void display();
+private:
+    std::map<Vertex<T>*, std::vector<Vertex<T>*>> _graph;
+    std::set<Vertex<T>*> vertices;
 };
 
 #include "../source/dfs_graph.tpp"
