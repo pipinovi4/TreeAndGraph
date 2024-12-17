1bbf39499e1b52d21034c8cc515750053ea9f489
pipinovi4 <prustolivgra@gmail.com>
2024-12-17 15:16:00 +0100

    Add BFSGraph class header file with BFS functionality in C++
    - Implemented `BFSGraph` class template with the following methods:
      - `add_vertex(Vertex<T>* vertex)`: Adds a vertex to the graph.
      - `add_edge(Vertex<T>* source, Vertex<T>* target)`: Adds a directed edge between two vertices, ensuring vertices are added if not present.
      - `bfs(Vertex<T>* start_vertex)`: Performs a Breadth-First Search (BFS) traversal starting from the specified vertex.
      - `display()`: Displays the adjacency list of the graph.
    
    - Implemented `Vertex` class template with the following methods:
      - `addNeighbor(Vertex<T>* neighbor)`: Adds a neighbor to the vertex.
      - `getValue()`: Returns the value of the vertex.
      - `getNeighbors()`: Returns the list of neighboring vertices.
    
    - Included guards (`#ifndef`, `#define`, `#endif`) to prevent multiple inclusions.
    - Added `#pragma once` for additional header protection.
    - Included necessary libraries: `<map>`, `<vector>`, `<set>`, and `<iostream>`.
    
    This commit adds the foundational structure for BFSGraph with proper encapsulation and modular design.

--- Graphs/BFSGraphs/include/bfs_graph.h
+++ Graphs/BFSGraphs/include/bfs_graph.h
@@ -2,11 +2,38 @@
 #define BFS_GRAPH_H
 
 #pragma once
+#include <map>
+#include <iostream>
+#include <vector>
+#include <set>
 
-class BFSGraph {
+template <typename T>
+class Vertex {
 public:
+    Vertex(T value);
+
+    void addNeighbor(Vertex<T>* neighbor);
+
+    const T& getValue() const;
+
+    const std::vector<Vertex<T>*>& getNeighbors() const;
 
 private:
+    T _value;
+    std::vector<Vertex<T>*> _neighbors;
+};
+
+template <typename T>
+class BFSGraph {
+public:
+    BFSGraph();
+    void add_vertex(Vertex<T>* vertex);
+    void add_edge(Vertex<T>* source, Vertex<T>* target);
+    void bfs(Vertex<T>* start_vertex);
+    void display();
+private:
+    std::map<Vertex<T>*, std::vector<Vertex<T>*>> _graph;
+    std::set<Vertex<T>*> _vertices;
 };
 
 #include "../source/bfs_graph.tpp"
