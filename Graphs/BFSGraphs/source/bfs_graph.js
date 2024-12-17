5e3122d865517c7b3f3b1619771e737e33871b3e
pipinovi4 <prustolivgra@gmail.com>
2024-12-17 14:44:52 +0100

    Implement BFSGraph class in JavaScript
    - Created `BFSGraph` class with the following methods:
      - `addVertex(vertex)`: Adds a vertex to the graph.
      - `addEdge(source, target)`: Adds a directed edge from `source` to `target`. If the vertices do not exist, they are added automatically.
      - `bfs(startVertex)`: Performs a Breadth-First Search (BFS) starting from `startVertex` and prints the traversal order.
      - `display()`: Displays the graph structure as an adjacency list.
    
    - Utilized:
      - `Set` to manage unique vertices.
      - `Array` as a queue to perform BFS traversal.
      - `process.stdout.write` to display output in one line.
    
    - File location: `Graphs/BFSGraphs/source/bfs_graph.js`
    
    This commit adds BFS functionality in JavaScript for traversing and displaying directed graphs.

--- Graphs/BFSGraphs/source/bfs_graph.js
+++ Graphs/BFSGraphs/source/bfs_graph.js
@@ -1,8 +1,58 @@
 class BFSGraph {
     constructor() {
-
+        this.graph = {};
+        this.vertices = new Set();
     }
 
+    addVertex(vertex) {
+        this.vertices.add(vertex);
+    }
+
+    addEdge(source, target) {
+        if (!this.vertices.has(source)) {
+            this.addVertex(source);
+        }
+        if (!this.vertices.has(target)) {
+            this.addVertex(target);
+        }
+
+        if (!this.graph[source]) {
+            this.graph[source] = [];
+        }
+        this.graph[source].push(target);
+    }
+
+    bfs(startVertex) {
+        if (!this.vertices.has(startVertex)) {
+            console.log(`Vertex ${startVertex} does not exist in the graph`);
+            return;
+        }
+
+        const visited = new Set();
+        const queue = [startVertex];
+
+        while (queue.length !== 0) {
+            const currenVertex = queue.shift();
+
+            if (!visited.has(currenVertex)) {
+                visited.add(currenVertex);
+                process.stdout.write(currenVertex + " ");
+
+                for (const neighbor of this.graph[currenVertex]) {
+                    if (!visited.has((neighbor))) {
+                        queue.push(neighbor);
+                    }
+                }
+            }
+        }
+        process.stdout.write("\n");
+    }
+
+    display() {
+        for (const vertex of this.vertices) {
+            console.log(`${vertex} -> ${this.graph[vertex]}`)
+        }
+    }
 }
 
 
