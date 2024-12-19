6496cdeed43073dd27747885f59d0da703411cf7
pipinovi4 <prustolivgra@gmail.com>
2024-12-19 12:44:43 +0100

    Added `DirectedAcyclicGraph` class template with enhanced type flexibility and functionality
    - **Header File (`directed_acyclic_graph.h`)**:
      - Implemented the `Vertex` class template for representing graph vertices.
      - Added `DirectedAcyclicGraph` class template with secondary typename for flexible return types.
      - Added methods:
        - `addVertex()`: Adds a vertex to the graph.
        - `addEdge()`: Adds a directed edge and checks for cycles.
        - `dfsIterative()`: Performs iterative DFS traversal with options for processing, recording stack, and cycle detection.
        - `topologicalSort()`: Returns vertices in topologically sorted order.
        - `display()`: Displays the graph structure.
      - Added `_hasCycle()` private method for cycle detection.
    
    - Included `#pragma once` for header guards and necessary standard libraries for functionality.
    - Added `#include "../source/directed_acyclic_graph.tpp"` for implementation details.

--- Graphs/DirectedAcyclicGraphs/include/directed_acyclic_graph.h
+++ Graphs/DirectedAcyclicGraphs/include/directed_acyclic_graph.h
@@ -2,12 +2,47 @@
 #define DIRECTED_ACYCLIC_GRAPH_H
 
 #pragma once
+#include <vector>
+#include <map>
+#include <stack>
+#include <set>
+#include <functional>
 
-class DirectedAcyclicGraph {
+template <typename T>
+class Vertex {
 public:
+    Vertex(T value);
+
+    void addNeighbor(Vertex<T>* neighbor);
+
+    const T& getValue() const;
+
+    const std::vector<Vertex<T>*>& getNeighbors() const;
 
 private:
+    T _value;
+    std::vector<Vertex<T>*> _neighbors;
+};
 
+template <typename T, typename U>
+class DirectedAcyclicGraph {
+public:
+    DirectedAcyclicGraph();
+    ~DirectedAcyclicGraph();
+
+    void addVertex();
+    void addEdge();
+
+    U dfsIterative(Vertex<T>* start_vertex, std::vector<Vertex<T>*>* visited=nullptr, std::function<void()>* process= nullptr, std::stack<Vertex<T>*>* record_stack=nullptr, bool detect_cycle=false);
+
+    std::vector<Vertex<T>*> topologicalSort();
+
+    void display();
+private:
+    std::map<Vertex<T>*, std::vector<Vertex<T>*>> _graph;
+    std::set<Vertex<T>*> _vertices;
+
+    bool _hasCycle();
 };
 
 #include "../source/directed_acyclic_graph.tpp"
